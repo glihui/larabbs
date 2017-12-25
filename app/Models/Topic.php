0d2700b61b1b913aeafd9df122284fe039404dc3
guolihui <15919476778@163.com>
2017-12-25 15:40:25 +0800

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,6 +6,11 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
@@ -45,4 +50,5 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
 }
